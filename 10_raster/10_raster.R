9f109a1a4dd387dd136ffa808f35f89b24c0b0ac
schochastics <david@schochastics.net>
2021-11-17 21:57:25 +0000

    code cleanup till day17

--- 10_raster/10_raster.R
+++ 10_raster/10_raster.R
@@ -1,17 +1,18 @@
 # https://earthobservatory.nasa.gov/images/3581/topography-of-south-america
 library(terra)
 library(tmap)
-SA <- rast("PIA03388.tif")#NE2_HR_LC_SR_W.tif")#NE2_HR_LC_SR.tif")
+SA <- rast("PIA03388.tif") 
 
 map1 <- tm_shape(SA) +
-  tm_rgb(r=1,g=2,b=3)+
-  tm_layout(sepia.intensity	=1,saturation = 1,frame = TRUE, 
-            main.title = "Topology of South America",title = "(data from earthobservatory.nasa.gov)", 
-            title.position = c("LEFT","bottom"),title.size = 1.2, title.fontfamily = "VonDutch",
-            main.title.fontfamily = "VonDutch", main.title.size = 4,
-            bg.color = "#84240c",frame.double.line = TRUE,frame.lwd = 2)+
-  tm_credits(text = "@schochastics",fontfamily = "VonDutch",align = "right",size = 1.2)+
-  tm_compass(position = c("right","top"),type = "8star")
+  tm_rgb(r = 1, g = 2, b = 3) +
+  tm_layout(
+    sepia.intensity = 1, saturation = 1, frame = TRUE,
+    main.title = "Topology of South America", title = "(data from earthobservatory.nasa.gov)",
+    title.position = c("LEFT", "bottom"), title.size = 1.2, title.fontfamily = "VonDutch",
+    main.title.fontfamily = "VonDutch", main.title.size = 4,
+    bg.color = "#84240c", frame.double.line = TRUE, frame.lwd = 2
+  ) +
+  tm_credits(text = "@schochastics", fontfamily = "VonDutch", align = "right", size = 1.2) +
+  tm_compass(position = c("right", "top"), type = "8star")
 
-tmap_save(map1,filename = "10_raster_blank.png")
-
+tmap_save(map1, filename = "10_raster_blank.png")
